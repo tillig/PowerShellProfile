606a0f2def5f2145704648009437ba53604f20a1
Travis Illig <tillig@paraesthesia.com>
2019-04-05 12:02:01 -0700

    Refactored common profile items to central script.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,46 +1,4 @@
-﻿Import-Module PSReadline
-Import-Module Microsoft.PowerShell.Archive
-Import-Module Pscx -NoClobber
-Import-Module posh-git
-Import-Module VSSetup
-Import-Module Illig
-
-# Windows defaults to ASCII
-Set-ConsoleEncoding -UTF8
-$Env:PYTHONIOENCODING = "UTF-8"
-
-# Import VS environment
-Invoke-VisualStudioDevPrompt
-
-# Aliases
-Set-Alias -Name which -Value Get-Command
-
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-
-# PowerShell parameter completion shim for the dotnet CLI
-Get-Command dotnet -ErrorAction Ignore | Out-Null
-if ($?) {
-  Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-    param($commandName, $wordToComplete, $cursorPosition)
-    dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
-      [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-    }
-  }
-}
-
-# Only try enabling bash completions if bash/Git for Windows is here.
-$enableBashCompletions = ($Null -ne (Get-Command bash -ErrorAction Ignore)) -or ($Null -ne (Get-Command git -ErrorAction Ignore))
-if ($enableBashCompletions) {
-  Import-Module PSBashCompletions
-  $completionPath = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($profile), "bash-completion")
-  Register-BashArgumentCompleter kubectl "$completionPath/kubectl_completions.sh"
-  Register-BashArgumentCompleter git "$completionPath/git_completions.sh"
-  Register-BashArgumentCompleter helm "$completionPath/helm_completions.sh"
-}
+﻿& $PSScriptRoot/ProfileCommon.ps1
 
 ###
 # Custom prompt adapted from Brad Wilson
