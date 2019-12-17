2577cb46f29530cdb068a2f4b1dd14e4e1ffd077
Travis Illig <travis.illig@fiserv.com>
2019-12-17 11:38:13 -0800

    Added script to install required modules.

---
+++ Install-ProfileModules.ps1
@@ -0,0 +1,23 @@
+$releaseModules = (
+    "PSReadline",
+    "Microsoft.PowerShell.Archive",
+    "posh-git",
+    "oh-my-posh",
+    "Pscx",
+    "PSBashCompletions",
+    "VSSetup"
+)
+
+# Settings for posh-git require v1.0.0 minimum, in pre-release
+# since 1/10/2018.
+$preReleaseModules = (
+    "posh-git"
+)
+
+$releaseModules | ForEach-Object {
+    Install-Module $_ -Scope CurrentUser -AllowClobber
+}
+
+$preReleaseModules | ForEach-Object {
+    Install-Module $_ -Scope CurrentUser -AllowClobber -AllowPrerelease
+}
