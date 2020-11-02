eccb9b917e4007bad2871c81f7e2a042e5c69b46
Travis Illig <tillig@paraesthesia.com>
2020-11-02 07:21:42 -0800

    Switch to oh-my-posh v3.

--- Install-ProfileModules.ps1
+++ Install-ProfileModules.ps1
@@ -10,7 +10,8 @@ $releaseModules = (
 # Settings for posh-git require v1.0.0 minimum, in pre-release
 # since 1/10/2018.
 $preReleaseModules = (
-    "posh-git"
+    "posh-git",
+    "oh-my-posh"
 )
 
 Write-Host "Installing release modules - watch for warnings, you may need to install a module and include -Force to get side-by-side support."
@@ -22,6 +23,3 @@ Write-Host "Installing prerelease modules - watch for warnings, you may need to 
 $preReleaseModules | ForEach-Object {
     Install-Module $_ -Scope CurrentUser -AllowClobber -AllowPrerelease
 }
-
-# Force a specfic version for oh-my-posh because v3 has issues in PowerShell.
-Install-Module oh-my-posh -RequiredVersion "2.0.487" -Scope CurrentUser -AllowClobber -AllowPrerelease
--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,9 @@
 ﻿& $PSScriptRoot/ProfileCommon.ps1
 
-# oh-my-posh v2
-$GetLocationStackCount = { (Get-Location -Stack).Count }
-Set-Theme Illig
-
-# oh-my-posh v3 - 10/21/2020 has some known PowerShell issues
-## https://github.com/JanDeDobbeleer/oh-my-posh3/issues/65
-# function Set-PoshContextEnvironment {
-#     $stackDepth = (Get-Location -Stack).Count
-#     $env:LOCATION_STACK_DEPTH = @{ $true = ''; $false = "$stackDepth" }[0 -eq $stackDepth];
-# }
-# New-Alias -Name 'Set-PoshContext' -Value 'Set-PoshContextEnvironment' -Scope Global
-# Set-PoshPrompt -Theme $PSScriptRoot/themes/illig.json
+# oh-my-posh v3
+function Set-PoshContextEnvironment {
+    $stackDepth = (Get-Location -Stack).Count
+    $env:LOCATION_STACK_DEPTH = @{ $true = ''; $false = "$stackDepth" }[0 -eq $stackDepth];
+}
+New-Alias -Name 'Set-PoshContext' -Value 'Set-PoshContextEnvironment' -Scope Global
+Set-PoshPrompt -Theme $PSScriptRoot/themes/illig.json
