8a7e38d6ab2aae3fad304870b3b8c17a7e9a3893
Travis Illig <tillig@paraesthesia.com>
2024-02-14 07:43:03 -0800

    Show when pre-commit is installed.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,21 +1,35 @@
 ﻿. $PSScriptRoot/ProfileCommon.ps1
 
 # oh-my-posh v3
+# This will run every time the prompt displays so it's important to keep it fast.
 function Set-PoshContextEnvironment {
+    # Enable the git segment to indicate if pre-commit is installed.
+    If (Get-Command git -ErrorAction SilentlyContinue) {
+        $repoRoot = git rev-parse --show-toplevel 2>&1
+        If($LASTEXITCODE -eq 0) {
+            $preCommitHook = Test-Path (Join-Path $repoRoot '.git' 'hooks' 'pre-commit')
+            $env:PRE_COMMIT_INSTALLED = @{ $true = '✓'; $false = '' }[$preCommitHook];
+        }
+    }
+
+    # Enable the pushd/popd stack depth to be displayed.
     $stackDepth = (Get-Location -Stack).Count
     $env:LOCATION_STACK_DEPTH = @{ $true = ''; $false = "$stackDepth" }[0 -eq $stackDepth];
 }
 
-if ($null -ne (Get-Command "oh-my-posh" -ErrorAction Ignore)) {
+If ($null -ne (Get-Command "oh-my-posh" -ErrorAction Ignore)) {
     oh-my-posh init pwsh --config $PSScriptRoot/themes/illig.json | Invoke-Expression
     New-Alias -Name 'Set-PoshContext' -Value 'Set-PoshContextEnvironment' -Scope Global
-} else {
+}
+Else {
     Write-Warning "oh-my-posh not detected. Install to get the prompt: https://ohmyposh.dev/docs/"
 }
-if (Get-Command kubectl -ErrorAction SilentlyContinue) {
+
+If (Get-Command kubectl -ErrorAction SilentlyContinue) {
     kubectl completion powershell | Out-String | Invoke-Expression
 }
+
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
+If (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
 }
